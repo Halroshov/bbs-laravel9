44a3df5be3adb0750d2cc8116d6ded1516de3151
Halroshov <sakurakatsuya@outlook.com>
2024-08-20 13:33:16 +0900

    基础页面架构

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function root(): Factory|View|Application
+    {
+        return view('pages.root');
+    }
 }
