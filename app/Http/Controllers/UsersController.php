cfa924aa778a5bd5e193b7beb79e33e2a601a644
Halroshov <sakurakatsuya@outlook.com>
2024-08-25 15:00:24 +0900

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,7 +47,7 @@ class UsersController extends Controller
         $data = $request->all();
 
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            $result = $uploader->save($request->avatar, 'avatars', $user->id，416);
             if ($result) {
                 $data['avatar'] = $result['path'];
             }
