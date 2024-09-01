0ab572bd7876bb6dddecb5493670f40f85d1f755
Halroshov <sakurakatsuya@outlook.com>
2024-09-01 18:36:22 +0900

    用户发表评论

--- routes/web.php
+++ routes/web.php
@@ -60,4 +60,4 @@ Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show
 
 // 话题表单中的图片上传
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
-Route::resource('replies', 'RepliesController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
