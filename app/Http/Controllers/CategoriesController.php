59970acbd88a73d70290f0017b68e4b8fdf26a6b
Halroshov <sakurakatsuya@outlook.com>
2024-09-09 16:07:45 +0900

    分类话题列表侧标让资源链接显示

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
@@ -18,10 +19,11 @@ class CategoriesController extends Controller
      * @param Category $category
      * @param Request $request
      * @param Topic $topic
+     * @param Link $link
      * @param User $user
      * @return Application|Factory|View
      */
-    public function show(Category $category, Request $request, Topic $topic, User $user): View|Factory|Application
+    public function show(Category $category, Request $request, Topic $topic, User $user, Link $link): View|Factory|Application
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)
@@ -31,8 +33,10 @@ class CategoriesController extends Controller
 
         // 活跃用户列表
         $active_users = $user->getActiveUsers();
+         // 资源链接
+        $links = $link->getAllCached();
 
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category', 'active_users'));
+        return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
     }
 }
